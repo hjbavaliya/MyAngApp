817867b619ceb6c7c3dcfb078e2ffd21ce227aa6
Hitesh <hjbavaliya@gmail.com>
2022-01-01 15:21:04 +0530

    test

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  count: number = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  UpdateCount() {
+    this.count += 1;
+  }
+
+}
